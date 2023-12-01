193dd8bda51259e7ff54e5219fdb594954bd1fcb
HendrikPeru <hendrik.peru@gmail.com>
2023-12-01 11:34:23 +0200

    commit

--- skript.js
+++ skript.js
@@ -3,7 +3,7 @@ var hetkePilt = 0;
 var intervalliPikkus = 5000;
 
 function vahetaTaust() {
-    document.getElementById('muutuv-taust').style.backgroundImage = (taustapildid[hetkePilt]);
+    document.getElementById('muutuv-taust').style.backgroundImage = taustapildid[hetkePilt];
     hetkePilt = (hetkePilt + 1) % taustapildid.length;
 }
 
